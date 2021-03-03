7ad54b308c2a98841826eaf7fd87ba65c167815d
Nipun Rohilla <Nipun.Rohilla77@gmail.com>
2021-03-03 12:21:37 +0530

    First commit

---
+++ src/main/java/com/appdeveloper/photoapp/api/gateway/PhotoAppApiGateway/PhotoAppApiSpringBootApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.appdeveloper.photoapp.api.gateway.PhotoAppApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiSpringBootApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiSpringBootApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appdeveloper/photoapp/api/gateway/PhotoAppApiGateway/PhotoAppApiSpringBootApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdeveloper.photoapp.api.gateway.PhotoAppApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiSpringBootApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
